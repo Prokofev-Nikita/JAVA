abd208db7feac3797ee6c6589e94fcab84bdc043
Prokofev-Nikita <prokofevnikita1@gmail.com>
2024-02-19 18:03:05 +0300

    Init commit

---
+++ src/main/java/by/bsuir/prokofyev/pnayavyproject/PnayavyProjectApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.prokofyev.pnayavyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PnayavyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PnayavyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/bsuir/prokofyev/pnayavyproject/PnayavyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package by.bsuir.prokofyev.pnayavyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PnayavyProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
