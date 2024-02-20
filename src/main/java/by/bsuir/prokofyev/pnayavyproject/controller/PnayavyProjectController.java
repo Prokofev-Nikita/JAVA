f4eb135284f1b1ebe8c3f5cc0fbdab35470914c9
Prokofev-Nikita <prokofevnikita1@gmail.com>
2024-02-20 11:25:34 +0300

    New new commit

--- src/main/java/by/bsuir/prokofyev/pnayavyproject/controller/PnayavyProjectController.java
+++ src/main/java/by/bsuir/prokofyev/pnayavyproject/controller/PnayavyProjectController.java
@@ -1,11 +1,11 @@
-package by.bsuir.prokofyev.pnayavyproject.server;
+package by.bsuir.prokofyev.pnayavyproject.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class BinanceServer {
+class BinanceServerController {
     @GetMapping("/hello")
     public String sayHello(@RequestParam(required = false) String name) {
         return name == null ? "You are noname, looser" : "58";
