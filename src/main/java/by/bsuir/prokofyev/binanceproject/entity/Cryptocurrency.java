a4241c9e403e0dd9340180214349a25460949673
Prokofev-Nikita <prokofevnikita1@gmail.com>
2024-03-18 23:56:33 +0300

    feat: add ManyToMany inside Cryptocurrency

--- src/main/java/by/bsuir/prokofyev/binanceproject/entity/Cryptocurrency.java
+++ src/main/java/by/bsuir/prokofyev/binanceproject/entity/Cryptocurrency.java
@@ -3,6 +3,8 @@ package by.bsuir.prokofyev.binanceproject.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Entity
 public class Cryptocurrency {
@@ -11,4 +13,8 @@ public class Cryptocurrency {
     private long id;
     private String name;
     private double price;
+
+    @ManyToMany
+    @JoinTable(name = "wallet", joinColumns = @JoinColumn(name = "amount_id"), inverseJoinColumns = @JoinColumn(name = "owner_id"))
+    List<User> users;
 }
