d8156a5d08156d30a2ff15893efd371613e4134f
Prokofev-Nikita <prokofevnikita1@gmail.com>
2024-04-04 18:06:53 +0300

    feat: update to JpaRepo, add @Query with RestResource

--- src/main/java/by/bsuir/prokofyev/binanceproject/api/UserCrud.java
+++ src/main/java/by/bsuir/prokofyev/binanceproject/api/UserCrud.java
@@ -1,13 +1,18 @@
 package by.bsuir.prokofyev.binanceproject.api;
 
 import by.bsuir.prokofyev.binanceproject.entity.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-public interface UserCrud extends CrudRepository<User, Long> {
+public interface UserCrud extends JpaRepository<User, Long> {
+
     List<User> findAllByIdIn(@Param("ids") List<Long> ids);
 
+    @RestResource(path = "names")
+    @Query("SELECT u FROM User u WHERE u.username LIKE %:username%")
     List<User> findAllByUsernameContainsIgnoreCase(@Param("username") String username);
 }
